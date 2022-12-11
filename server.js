123c9a021c08d48f03582b4e8da4ad3a05094c34
nikkikhou <nikkikhou@gmail.com>
2022-12-11 11:10:29 -0500

    added stub methods to list/modify employees

--- server.js
+++ server.js
@@ -1,15 +1,7 @@
-const express = require('express');
 const mysql = require('mysql2');
 const inquirer = require('inquirer')
 const consoletable = require('console.table')
 
-
-const PORT = process.env.PORT || 3001;
-const app = express();
-
-// Express middleware
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
 
 // Connect to database
 const db = mysql.createConnection(
@@ -22,4 +14,53 @@ const db = mysql.createConnection(
   console.log(`Connected to the employee_db database.`)
 );
 
-// TODO: Create an array of questions for user input
+// function which determines action user wishes to take 
+// (view all departments, view all roles, view all employees, add a department, add a role, add an employee, and update an employee role)
+
+// function which view all departments
+function viewAllDepartments(){
+  return []
+}
+
+// function which view all roles
+function viewAllRoles(){
+  return []
+}
+
+// function which adds a department
+function addDepartment(){
+  return {}
+}
+
+// function which adds a role
+function addRole(){
+  return {}
+}
+
+// function which adds an Employee
+function addEmployee(){
+  return {}
+}
+// function which updates an employee role
+function updateRole(){
+  return {}
+}
+
+
+// function init() {
+//   inquirer
+//     .prompt(questions)
+//     .then((answers) => {
+//       writeToFile("README.md", generateMarkdown(answers));
+//     })
+//     .catch((error) => {
+//       if (error.isTtyError) {
+//         console.log("Could not be rendered")
+//       } else {
+//        console.log("Please enter more")
+//       }
+//     });
+// }
+
+// // Function call to initialize app
+// init();
